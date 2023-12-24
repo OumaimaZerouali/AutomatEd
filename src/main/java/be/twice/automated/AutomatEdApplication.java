24621cb897dd5faad3a074f3ca0a0ea10d7913ef
OumaimaZerouali <oumaima.zerouali@ordina.be>
2023-12-24 22:04:11 +0100

    Initial Commit

---
+++ src/main/java/be/twice/automated/AutomatEdApplication.java
@@ -0,0 +1,13 @@
+package be.twice.automated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatEdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatEdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/twice/automated/AutomatEdApplicationTests.java
@@ -0,0 +1,13 @@
+package be.twice.automated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatEdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
