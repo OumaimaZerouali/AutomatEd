4538808a90fa09ad2405e8267f47307258fd8df9
GabrielDelaPena <89069807+GabrielDelaPena@users.noreply.github.com>
2023-12-26 11:37:47 +0100

    Initialize spring security configuration and added password hashing.

--- user/src/main/java/be/twice/service/UserService.java
+++ user/src/main/java/be/twice/service/UserService.java
@@ -4,24 +4,30 @@ import be.twice.model.Role;
 import be.twice.model.User;
 import be.twice.model.UserDTO;
 import be.twice.repository.UserRepository;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    @Autowired
+    public UserService(UserRepository repository, BCryptPasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(UserDTO userDTO) {
+        String hashedPassword = passwordEncoder.encode(userDTO.getPassword());
+
         User user = new User();
         user.setEmail(userDTO.getEmail());
-        user.setPassword(userDTO.getPassword());
         user.setUsername(userDTO.getUsername());
         user.setRole(Role.USER);
+        user.setPassword(hashedPassword);
 
         repository.insert(user);
         return user;
